3ae998d7739e86e4aabef8c4b976877455830e90
nour al-Zaghloul <nouralzaghloul@gmail.com>
2022-01-11 09:20:53 -0500

    Activity Logger MVP

---
+++ src/App.svelte
@@ -0,0 +1,191 @@
+<script>
+	let activities = JSON.parse(localStorage.getItem('activityLogger-nourthepigeon')) || [{id: uuidv4(), name: 'Use Activity Logger', time: 0, dateStarted: getDate(), active: true, datesWorkedOn: []}];
+	//let activities = [{id: 0, name: 'Jogging', time: 50000, dateStarted: '2022-01-10', active: true, datesWorkedOn: []}];
+	let selectedActivity = undefined;
+	let selectedIndex = undefined;
+	
+	let playPause = false;
+	let lastTime = 0;
+	let currentTime = 0;
+	let deltaTime = 0;
+	let delayTime = 0;
+	let loopAF;
+	
+	function uuidv4() {
+	  	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+	    	(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+	  	);
+	}
+
+	function timerToggle(){
+		if(playPause == false){
+			let dates = new Set(selectedActivity.datesWorkedOn);
+			dates.add(getDate());
+			selectedActivity.datesWorkedOn = Array.from(dates);
+			
+			loopAF = window.requestAnimationFrame(timer);
+		} else {
+			window.cancelAnimationFrame(loopAF);
+			lastTime = 0;
+			currentTime = 0;
+			deltaTime = 0;
+			
+			saveActivities();
+		}		
+		playPause = !playPause;
+	}
+	
+	function timer(time){
+		lastTime = currentTime;
+		currentTime = time || 0;
+		
+		if(lastTime !== 0){
+			deltaTime = currentTime - lastTime;
+			delayTime += deltaTime;
+			selectedActivity.time = selectedActivity.time + deltaTime;
+		} 
+		
+		if(delayTime >= 60000){
+			saveActivities();
+		}
+		
+		loopAF = window.requestAnimationFrame(timer);
+	}
+	
+	function saveActivities(){
+			delayTime = 0;
+			localStorage.setItem('activityLogger-nourthepigeon', JSON.stringify(activities));
+	}
+	
+	function selectActivity(e){
+		for(let i=0; i<e.path.length; i++){
+			if (e.path[i].className.includes('activityContainer')) {
+				selectedIndex = activities.findIndex(x => x.id == e.path[i].id);
+
+				selectedActivity = activities[selectedIndex];
+				break;
+			}
+		}
+	}
+	
+	function deselectActivity(){
+		if(playPause == true){
+			timerToggle();
+		} else {
+			saveActivities();
+		}
+		selectedActivity = undefined;
+	}
+	
+	function removeActivity(e){
+		activities = activities.filter(x => x.id != e.path[1].id);
+		saveActivities();
+	}
+	
+	function addActivity(e){
+		let newActivity = document.getElementById("activityName");
+		if(newActivity.value !== ""){
+			activities.push({
+				id: uuidv4(),
+				name: newActivity.value,
+				time: 0,
+				dateStarted: getDate(),
+				active: true,
+				datesWorkedOn: [],
+			})
+			activities = activities;
+		} else {
+			console.log('empty')
+		}
+		
+		newActivity.value = "";
+		
+		saveActivities();
+	}
+	
+	function getDate(){
+		let newTime = new Date();
+		newTime = newTime.toISOString();
+		newTime = newTime.match(/.*(?=T)/)[0];
+		return newTime
+	}
+	
+	function msToTime(duration) {
+  	var milliseconds = Math.floor((duration % 1000) / 100),
+    	seconds = Math.floor((duration / 1000) % 60),
+    	minutes = Math.floor((duration / (1000 * 60)) % 60),
+    	hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+  	hours = (hours < 10) ? "0" + hours : hours;
+  	minutes = (minutes < 10) ? "0" + minutes : minutes;
+  	seconds = (seconds < 10) ? "0" + seconds : seconds;
+		
+  	return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+	}
+</script>
+
+<div>
+	<h1>
+		Activity Logger
+	</h1>
+	{#if selectedActivity == undefined}
+	<h3>
+		Active
+	</h3>
+	{#each activities as activity (activity.id)}
+	<div class="activityContainer" id={activity.id} on:click={selectActivity}>
+		<button class="badbutton" id="remove" on:click={removeActivity}>Trash</button>
+		<h4>
+			{activity.name}
+		</h4>
+		<p>
+			Date Started: {activity.dateStarted}
+			<br>
+			Total Time: {msToTime(activity.time)}
+		</p>
+		<!--<button id={activity.id} on:click={selectActivity}>Open!</button>-->
+	</div>
+	<br>
+	{/each}
+	<div class="activityContainer">
+		<form>
+			<label>
+				<h4>
+					New Activity:
+				</h4>
+				<input id="activityName">
+			</label>
+		</form>
+		<br>
+		<button on:click={addActivity}>+</button>
+	</div>
+	{:else}
+	<div class="activityContainer" id="selectedActivity">
+		<button class="badbutton" id='deselect' on:click={deselectActivity}>X</button>
+		<h3>{selectedActivity.name}</h3>
+			<h4>{msToTime(selectedActivity.time)}</h4>
+			<p>Total Days: {selectedActivity.datesWorkedOn.length}</p>
+			{#if playPause == false}
+			<button on:click={timerToggle}>Start Timer</button>
+			{:else}
+			<button on:click={timerToggle}>Stop Timer</button>
+			{/if}
+	</div>
+	{/if}
+</div>
+
+<style>
+	.activityContainer {
+		position: relative;
+		background-color: lightblue;
+		border: 2px black solid;
+		text-align: center;
+	}
+	
+	.badbutton {
+		position: absolute;
+		left: 0;
+		top: 0;
+	}
+
+</style>
