f6058dee5a185d22b348fee7b32b504e4b418d28
nour al-Zaghloul <nouralzaghloul@gmail.com>
2022-01-11 09:45:44 -0500

    random page post

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+let projects = [
+    {name: 'Activity Logger', url: "https://www.pigeonworkshop.com/activityLogger"}
+]
+
+let iframe = document.getElementById('preview');
+
+function grabRandomProject(){
+    let index = Math.floor(Math.random() * (projects.length - 1 + 1));
+    console.log(projects[index].name);
+    iframe.src = projects[index].url;
+    return index
+}
