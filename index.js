c290d0166601e793c652ebe5a7e3a19e901db292
nour al-Zaghloul <nouralzaghloul@gmail.com>
2022-04-10 14:02:32 -0400

    redo

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let projects = [
-    {name: 'Activity Logger', url: "activity-logger.pigeonworkshop.com/"}
+    {name: 'Activity Logger', url: `${window.location.href}activityLogger.html`},
+    {name: 'CSS Looks', url: `${window.location.href}cssLooks.html`},
 ]
 
 let iframe = document.getElementById('preview');
 
 function grabRandomProject(){
-    let index = Math.floor(Math.random() * (projects.length - 1 + 1));
-    console.log(projects[index].url);
+    let index = Math.floor(Math.random() * (projects.length));
+    console.log(`${projects[index].name}: ${projects[index].url}`);
     iframe.src = projects[index].url;
-    console.log(iframe.src)
+    if(iframe.hidden){iframe.hidden = false}
     return index
 }
