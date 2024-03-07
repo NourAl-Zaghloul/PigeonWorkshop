9ff7ce433cfb101587154cefa5a828de2fa1b674
nour al-Zaghloul <nouralzaghloul@gmail.com>
2024-03-06 21:57:18 -0500

    update css

--- assets/js/cssPicker.js
+++
@@ -1,8 +0,0 @@
-let selectTheme = document.getElementById('changeCSS');
-
-function changeCSS(){
-    console.log(selectTheme.value);
-    document.documentElement.className = selectTheme.value;
-}
-
-selectTheme.addEventListener('change', changeCSS);
