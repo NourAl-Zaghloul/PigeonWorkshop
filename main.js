aa0607194dccce3ccd9a4014b12f655f86a50549
nour al-Zaghloul <nouralzaghloul@gmail.com>
2024-03-08 22:57:45 -0500

    Fixing up -- nearly there...

---
+++ main.js
@@ -0,0 +1,23 @@
+let bullshit = [
+    {text: "oh ho", value: "yup"}
+];
+
+
+
+function randomBullshit(){
+    window.location.assign(`./bullshit/${bullshit[Math.floor(Math.random() * bullshit.length)].value}`);
+}
+
+function curatedBullshit(){
+    let bullshitDOM = document.getElementById("selectBullshit")
+    for(let i = 0; i < bullshit.length; i++){
+        let bullshitOption = new Option(bullshit[i].text, bullshit[i].value);
+        bullshitDOM.add(bullshitOption);
+    }
+    console.log("there's your bullshit");
+}
+
+function selectBullshit(){
+    let bullshitDOM = document.getElementById("selectBullshit");
+    window.location.assign(`./bullshit/${bullshitDOM.value}`);
+}
